01996c477c6bccc9a3f85ad846fd19b11d852ccc
Tim Rogers <timr36@comcast.net>
2018-10-12 22:03:50 -0700

    Add submit event to form to launch makeGrid

--- designs.js
+++ designs.js
@@ -1,17 +1,34 @@
 // Select color input
+
+
 // Select size input
+var inputHeight, inputWidth;
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(rows, columns) {
+const formClick = document.querySelector('#sizePicker');
+formClick.addEventListener('submit', function () {
+  event.preventDefault();
+  makeGrid();
+});
+
+function makeGrid() {
+
   const pixelGrid = document.querySelector('#pixelCanvas');
+
+  pixelGrid.innerHTML = '';
+
+  inputHeight = document.querySelector('#inputHeight').value;
+  inputWidth = document.querySelector('#inputWidth').value;
+
   var tableHTML =''
-  for (var height = 0; height < rows; ++height) {
+  for (var height = 0; height < inputHeight; ++height) {
     tableHTML = tableHTML + '<tr>';
-    for (var width = 0; width < columns; ++width) {
+    for (var width = 0; width < inputWidth; ++width) {
       tableHTML = tableHTML + '<td></td>';
     }
     tableHTML = tableHTML + '</tr>';
   }
   pixelGrid.insertAdjacentHTML('beforeend', tableHTML);
+
 }
